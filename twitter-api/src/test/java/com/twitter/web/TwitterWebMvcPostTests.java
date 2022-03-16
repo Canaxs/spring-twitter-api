67b750eddc592bce8004d6b0efa7ac3181a23834
Meriç Cana <91698544+Canaxs@users.noreply.github.com>
2022-03-16 13:51:24 +0300

    post-unit-test-written
    MockMvc

---
+++ twitter-api/src/test/java/com/twitter/web/TwitterWebMvcPostTests.java
@@ -0,0 +1,83 @@
+package com.twitter.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.twitter.auth.AuthService;
+import com.twitter.post.PostService;
+import com.twitter.temporary.Credentials;
+import com.twitter.temporary.UserAuthRes;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@ActiveProfiles("dev")
+@AutoConfigureMockMvc
+public class TwitterWebMvcPostTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private AuthService authService;
+	
+	public String Auth() {
+		Credentials credentials = new Credentials();
+		credentials.setPassword("root");
+		credentials.setUsername("users");
+		UserAuthRes authRes = authService.authenticate(credentials);
+		return authRes.getToken();
+	}
+	
+	@Test
+	public void getUserPosts() throws Exception {
+		String format = Auth();
+		String username = "user";
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/1.0/"+username).header("Authorization",format))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	public void getAuthPost() throws Exception {
+		String format = Auth();
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/1.0/post").header("Authorization", format))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	public void createPost() throws Exception {
+		String format = Auth();
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/1.0/post")
+				.header("Authorization", format)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"title\": \"deneme\",\"text\": \"deneme\"}")
+				)
+		.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	public void deletePost() throws Exception {
+		String format =  Auth();
+		long id = 4;
+		mockMvc.perform(MockMvcRequestBuilders.delete("/api/1.0/post/"+id).header("Authorization", format))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	public void getFriendsPost() throws Exception{
+		String format = Auth();
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/1.0/post/friends").header("Authorization", format))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+
+}
